2e4672b96183f43929d589c79568b49a739fd278
DDavid <zjdavid.2003@gmail.com>
2021-07-10 20:19:27 +0800

    debug

--- setup_Windows/STEP2-install_components.ps1
+++ setup_Windows/STEP2-install_components.ps1
@@ -3,8 +3,6 @@ cd "${PSScriptRoot}\..\"
 conda create -n NLP -y
 conda activate NLP
 
-curl -o Anaconda3-Windows-x86_64.exe https://repo.anaconda.com/archive/Anaconda3-2021.05-Windows-x86_64.exe
-
 python -m pip install -r requirements.txt
 
 conda activate NLP
