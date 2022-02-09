a076b702677bb1f4521fdcd555dc2c6e68dfee85
cgong99 <gongcchen999@gmail.com>
2022-02-09 16:24:22 -0500

    basic query successed

--- src/knowledge_graphs_DBpedia_util_chen.py
+++ src/knowledge_graphs_DBpedia_util_chen.py
@@ -3,24 +3,24 @@
 import os
 import tkinter.messagebox as mb
 import string
-punksAndNum = string.punctuation + '1' + '2' + '3' + '4' + '5' + '6' + '7' + '8' + '9' + '0'
-
 import re
 from re import split
 from urllib import request, error
-
 import stanza
+from SPARQLWrapper import SPARQLWrapper, JSON, XML
+
+sparql = SPARQLWrapper("http://dbpedia.org/sparql")
+
 stanza.download('en')
 stannlp = stanza.Pipeline(lang='en', processors='tokenize,ner,mwt,pos,lemma')
 
-from SPARQLWrapper import SPARQLWrapper, JSON, XML
-sparql = SPARQLWrapper("http://dbpedia.org/sparql")
+punksAndNum = string.punctuation + '1' + '2' + '3' + '4' + '5' + '6' + '7' + '8' + '9' + '0'
 
 
-
-def yagao_annotate(inputFile, inputDir, outputDir, annotationTypes, color1, colorls):
-    contents = open(inputFile, 'r', encoding='utf-8', errors='ignore').read()
-    contents = preprocess_content(contents)
+# def yagao_annotate(inputFile, inputDir, outputDir, annotationTypes, color1, colorls):
+def yagao_annotate(contents, annotationTypes):
+    # contents = open(inputFile, 'r', encoding='utf-8', errors='ignore').read()
+    contents = preprocessing(contents)
     annotated_doc = stanford_annotator(contents)
     for sent_id in range(len(annotated_doc.sentences)):
         sent = annotated_doc.sentences[sent_id]
@@ -45,40 +45,70 @@ def yagao_annotate(inputFile, inputDir, outputDir, annotationTypes, color1, colo
 
     pass
 
+
 def annotate(phrase_og, phrase_tr, cats):
-    form_query_string(phrase_tr, cats)
+    query = form_query_string(phrase_tr, cats)
+    get_result(query)
     pass
 
 
 def form_query_string(phrase, ont_ls):
+    """
+
+    Parameters
+    ----------
+    phrase
+    ont_ls
+
+    Returns
+    -------
+
+    """
     query_body = ''
+    # "PREFIX https://dbpedia.org/snorql/"
+    # + 'PREFIX dbpedia2: <http://dbpedia.org/property/>' + '\n' \
     query_s = 'PREFIX owl: <http://www.w3.org/2002/07/owl#>' + '\n' \
-              + 'PREFIX schema: <http://schema.org/>' + '\n' \
-              + 'PREFIX bioschemas: <http://bioschemas.org/>' + '\n' \
-              + 'PREFIX yago: <http://yago-knowledge.org/resource/>' + '\n' \
-              + 'PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>' + '\n' \
+              + 'PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>' + '\n' \
               + 'PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>' + '\n' \
+              + 'PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>' + '\n' \
+              + 'PREFIX foaf: <http://xmlns.com/foaf/0.1/>' + '\n' \
+              + 'PREFIX dc: <http://purl.org/dc/elements/1.1/>' + '\n' \
+              + 'PREFIX : <http://dbpedia.org/resource/>' + '\n' \
+              + 'PREFIX dbpedia: <http://dbpedia.org/>' + '\n' \
+              + 'PREFIX skos: <http://www.w3.org/2004/02/skos/core#>' + '\n' \
               + 'SELECT DISTINCT'
-    query_s = query_s + '?' + 'w1'   # SELECT DISTINCT w1
+
+    query_s = query_s + ' ?' + 'w1'  # SELECT DISTINCT w1
     query_body = query_body + '?' + 'w1 ' + 'rdfs:label' + " \"" + phrase + "\"" + '@en'
+    query_body = query_body + ".\n" + "?w1 rdf:type " + ont_ls
     query_s = query_s + "\nWHERE { "
     query_s = query_s + query_body
     query_s = query_s + "}"
-    return
+    return query_s
 
 
 def get_result(query):
+    """
+    send query request and get the result
+    Parameters
+    ----------
+    query a string of query
+
+    Returns
+    -------
+    results
+    """
     sparql.setQuery(query)
     sparql.setReturnFormat(JSON)
     try:
-        results=sparql.query().convert()
+        results = sparql.query().convert()
     except error.HTTPError:
         # this may occasionally give time out error depending upon server's traffic
         mb.showwarning(title='Warning',
                        message='Take a look at your command line/prompt. An HTTP error of 500 means that the YAGO server failed. Please, check command line/prompt for "Operation timed out" error\n\nTry running the script later, when the server may be be less busy.')
         return None
+    # TODO process the result
     return results
-
 
 
 def check_eligible(phrase):
@@ -88,7 +118,17 @@ def check_eligible(phrase):
         return False
 
 
-def preprocess_content(contents):
+def preprocessing(contents):
+    """
+    Preprocess the contents by removing some special chars
+    Parameters
+    ----------
+    contents String, raw contents in the document
+
+    Returns
+    -------
+    contents cleaned
+    """
     contents = ' '.join(contents.split())  # reformat content
     contents = contents.replace('\0', '')  # remove null bytes
     contents = contents.replace('\'', '')  # remove quotation marks
@@ -100,3 +140,12 @@ def preprocess_content(contents):
 
 def stanford_annotator(content):
     return stannlp(content)
+
+
+# Testing
+if __name__ == '__main__':
+    contents = "New York. China"
+    annotationTypes = "dbo:Place"
+    phrase = "China"
+    query = form_query_string(phrase, annotationTypes)
+    res = get_result(query)
