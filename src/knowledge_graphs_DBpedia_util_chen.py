0df170329b263e8c0b13f628acb5a173617d6bc5
cgong99 <gongcchen999@gmail.com>
2022-02-19 14:51:27 -0500

    html update

--- src/knowledge_graphs_DBpedia_util_chen.py
+++ src/knowledge_graphs_DBpedia_util_chen.py
@@ -17,10 +17,46 @@ stannlp = stanza.Pipeline(lang='en', processors='tokenize,ner,mwt,pos,lemma')
 punksAndNum = string.punctuation + '1' + '2' + '3' + '4' + '5' + '6' + '7' + '8' + '9' + '0'
 
 
-# def yagao_annotate(inputFile, inputDir, outputDir, annotationTypes, color1, colorls):
-def yagao_annotate(contents, annotationTypes):
+# def DBpedia_annotate(inputFile, inputDir, outputDir, annotationTypes, color1, colorls):
+def DBpedia_annotate(contents, annotationTypes):
     # contents = open(inputFile, 'r', encoding='utf-8', errors='ignore').read()
     contents = preprocessing(contents)
+    html_str = annotate(contents, annotationTypes)
+    # TODO: create file
+    return html_str
+
+
+def annotate(contents, annotationTypes):
+    """
+    annotate the input contents. Using stanford annotator to filter out words.
+    NNP and NNPs will be grouped together if they are directly followed by each other
+
+    Parameters
+    ----------
+    contents: preprocessed file content
+    annotationTypes:
+
+    Returns
+    -------
+
+
+    """
+    color1 = 'black'
+    color2 = 'blue'
+    html_str = '<html>\n<body>\n<div>\n'
+    html_str_end = '\n</div>\n</body>\n</html>'
+
+    # tA1 = ['<span style=\"color: ' + color1 + '\">',
+    #         '</span> ']
+    # tA2 = ['<a style=\"color:' + color2 + '\" href=\"',
+    #        '\">',
+    #        '</a> ']
+
+    tA1 = ['<span>', '</span> ']
+    tA2 = ['<a href=\"',
+           '\">',
+           '</a> ']
+
     annotated_doc = stanford_annotator(contents)
     for sent_id in range(len(annotated_doc.sentences)):
         sent = annotated_doc.sentences[sent_id]
@@ -37,19 +73,26 @@ def yagao_annotate(contents, annotationTypes):
                 prev_tr = prev_tr + word.lemma + " "
                 prev_og = prev_og + word.text + " "
             else:  # annotate the Proper noun cached previously and the current word
-                annotate(prev_og[:-1], prev_tr[:-1], annotationTypes)
+                query_and_html(prev_og[:-1], prev_tr[:-1], annotationTypes)
                 prev_og = ""
                 prev_tr = ""
                 if check_eligible(str(word.text)) and pos:
-                    annotate(str(word.text), str(word.lemma), annotationTypes)
+                    query_and_html(str(word.text), str(word.lemma), annotationTypes)
 
-    pass
+    return html_str + html_str_end
 
 
-def annotate(phrase_og, phrase_tr, cats):
+def query_and_html(phrase_og, phrase_tr, cats, html_str, tA1, tA2):
     query = form_query_string(phrase_tr, cats)
-    get_result(query)
-    pass
+    res = get_result(query)
+    if not res:  # no url returned normal html, no annotation
+        html_str += tA1[0] + phrase_og + tA1[1]
+    else:  # annotate with the url
+        url = res[0]
+        html_str += tA2[0] + str(url) + tA2[1] + phrase_og + tA2[2]
+        ## TODO choose the best link
+
+    return html_str
 
 
 def form_query_string(phrase, ont_ls):
@@ -92,11 +135,11 @@ def get_result(query):
     send query request and get the result
     Parameters
     ----------
-    query a string of query
+    query: a string of query
 
     Returns
     -------
-    results
+    results a list of urls
     """
     sparql.setQuery(query)
     sparql.setReturnFormat(JSON)
@@ -107,8 +150,13 @@ def get_result(query):
         mb.showwarning(title='Warning',
                        message='Take a look at your command line/prompt. An HTTP error of 500 means that the YAGO server failed. Please, check command line/prompt for "Operation timed out" error\n\nTry running the script later, when the server may be be less busy.')
         return None
-    # TODO process the result
-    return results
+
+    # TODO process the result?
+    bindings = results['results']['bindings']  # [{w1: {type: uri, value: "http:/xxx"}}]
+    url_list = []
+    for link in bindings:
+        url_list.append(link['w1']['value'])
+    return url_list
 
 
 def check_eligible(phrase):
@@ -123,11 +171,11 @@ def preprocessing(contents):
     Preprocess the contents by removing some special chars
     Parameters
     ----------
-    contents String, raw contents in the document
+    contents: String, raw contents in the document
 
     Returns
     -------
-    contents cleaned
+    contents: cleaned
     """
     contents = ' '.join(contents.split())  # reformat content
     contents = contents.replace('\0', '')  # remove null bytes
@@ -144,8 +192,13 @@ def stanford_annotator(content):
 
 # Testing
 if __name__ == '__main__':
-    contents = "New York. China"
+    contents = "I went to NewYork. I am from China"
     annotationTypes = "dbo:Place"
-    phrase = "China"
-    query = form_query_string(phrase, annotationTypes)
-    res = get_result(query)
+    # phrase = "China"
+    # query = form_query_string(phrase, annotationTypes)
+    # res = get_result(query)
+    # data = res['results']['bindings']
+    # print(type(res))
+    html_str = DBpedia_annotate(contents, annotationTypes)
+    print(html_str)
+
