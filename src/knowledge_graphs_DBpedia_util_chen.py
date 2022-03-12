cfd3da6ba5e68fb11e60a9db5d6683cfa4a4bb2a
cgong99 <gongcchen999@gmail.com>
2022-03-11 21:44:59 -0500

    query for parent classes

--- src/knowledge_graphs_DBpedia_util_chen.py
+++ src/knowledge_graphs_DBpedia_util_chen.py
@@ -187,16 +187,17 @@ def form_query_string(phrase, ont_ls):
                 # + 'PREFIX dbpedia: <http://dbpedia.org/>' + '\n' \
                  # + 'PREFIX : <http://dbpedia.org/resource/>' + '\n' \
                 # 'PREFIX owl: <http://www.w3.org/2002/07/owl#>' + '\n' \
-    # TODO: query sub class
+    # TODO: check dbo:wikiPageDisambiguates for similar expression
     query_s = query_s + ' ?' + 'w1'  # SELECT DISTINCT w1
     query_body = query_body + '?' + 'w1 ' + 'rdfs:label' + " \"" + phrase + "\"" + '@en.\n'
     # query_body = query_body  + "?w1 rdf:type " + "dbo:" + ont_ls[0]
     for ont in ont_ls:
-        query_body = query_body + " { ?w1 rdf:type " + "dbo:" + ont + ' } UNION'
+        query_body = query_body + " { ?w1 a " + "dbo:" + ont + ' } UNION' + '{?w1 a ?type.\n ?type rdfs:subClassOf*  owl:' + ont+ '} UNION'
     query_body = query_body[:-5] # remove the last UNION
     query_s = query_s + "\nWHERE { "
     query_s = query_s + query_body
     query_s = query_s + "}"
+    print(query_s)
     return query_s
 
 
@@ -281,8 +282,8 @@ def stanford_annotator(content):
 # Testing
 if __name__ == '__main__':
     contents = "Atlanta. I am from China"
-    annotationTypes = ['Place']
-    inputFile = '/Users/gongchen/Emory_NLP/NLP-Suite/DBpedia_in/news.txt' # new copy.txt
+    annotationTypes = ['Thing']
+    inputFile = '/Users/gongchen/Emory_NLP/NLP-Suite/DBpedia_in/news copy.txt' # new copy.txt
     outputDir = '/Users/gongchen/Emory_NLP/NLP-Suite/DBpedia_out'
     inputDir = ''
 
